30831791d3f761725e3a3fa8800b7eb36f4db232
AliesYangpai <yangpai_beibei@163.com>
2021-08-04 23:07:09 +0800

    add CloudTagLayout

---
+++ module_practice_learn_view/src/main/java/com/alie/modulepracticelearnview/view/CloudTagLayout.kt
@@ -0,0 +1,28 @@
+package com.alie.modulepracticelearnview.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+/**
+ * CloudTagLayout
+ */
+class CloudTagLayout(context:Context, attrs: AttributeSet? = null, defStyleAttr:Int = 0, defStyleRes:Int = 0):
+    ViewGroup(context,attrs,defStyleAttr,defStyleRes) {
+
+    companion object {
+        const val TAG = "CloudTagLayout"
+    }
+
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+    }
+
+
+
+}
