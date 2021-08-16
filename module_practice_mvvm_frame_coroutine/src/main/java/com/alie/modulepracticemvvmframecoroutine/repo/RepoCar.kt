e12825a88da57bf850a075754acbc10b82c0d0f4
AliesYangpai <yangpai_beibei@163.com>
2021-08-16 21:14:54 +0800

    optimize coroutine

--- module_practice_mvvm_frame_coroutine/src/main/java/com/alie/modulepracticemvvmframecoroutine/repo/RepoCar.kt
+++ module_practice_mvvm_frame_coroutine/src/main/java/com/alie/modulepracticemvvmframecoroutine/repo/RepoCar.kt
@@ -6,15 +6,25 @@ import com.alie.modulepracticemvvmframecoroutine.data.local.CarBean
 import com.alie.modulepracticemvvmframecoroutine.data.local.CarFrameBean
 import com.alie.modulepracticemvvmframecoroutine.data.local.EngineBean
 import com.alie.modulepracticemvvmframecoroutine.data.http.HttpResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class RepoCar {
 
     private val mCarApi by lazy { ApiHttpClient.getInstance().generateService(ServiceCar::class.java) }
 
     suspend fun loadDataCar(): HttpResponse<CarBean?>? {
-        return mCarApi?.loadCar()
+        return withContext(Dispatchers.IO) {
+            mCarApi?.loadCar()
+        }
     }
-    suspend fun loadDataEngine(): HttpResponse<EngineBean?>? = mCarApi?.loadEngine()
-    suspend fun loadDataCarFrame(): HttpResponse<CarFrameBean?>? = mCarApi?.loadCarFrame()
-    suspend fun loadDataWheel() = mCarApi?.loadWheel()
+    suspend fun loadDataEngine(): HttpResponse<EngineBean?>? = withContext(Dispatchers.IO) {
+        mCarApi?.loadEngine()
+    }
+    suspend fun loadDataCarFrame(): HttpResponse<CarFrameBean?>? = withContext(Dispatchers.IO) {
+        mCarApi?.loadCarFrame()
+    }
+    suspend fun loadDataWheel() = withContext(Dispatchers.IO) {
+        mCarApi?.loadWheel()
+    }
 }
--- module_practice_mvvm_frame_coroutine/src/main/java/com/alie/modulepracticemvvmframecoroutine/repo/RepoWeather.kt
+++ module_practice_mvvm_frame_coroutine/src/main/java/com/alie/modulepracticemvvmframecoroutine/repo/RepoWeather.kt
@@ -2,9 +2,21 @@ package com.alie.modulepracticemvvmframecoroutine.repo
 
 import com.alie.modulepracticemvvmframecoroutine.api.ApiHttpClient
 import com.alie.modulepracticemvvmframecoroutine.api.service.ServiceWeather
+import com.alie.modulepracticemvvmframecoroutine.data.http.HttpThirdResponse
+import com.alie.modulepracticemvvmframecoroutine.data.third.WeatherBean
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class RepoWeather {
-    suspend fun loadWeather(cityCode: String = "101010100") =
+
+    private val mWeatherApi by lazy {
         ApiHttpClient.getInstance().generateService(ServiceWeather::class.java)
-            ?.loadWeather(cityCode)
+    }
+
+    suspend fun loadWeather(cityCode: String = "101010100") = withContext(Dispatchers.IO) {
+        mWeatherApi?.loadWeather(cityCode)
+    }
+    
+
+
 }
