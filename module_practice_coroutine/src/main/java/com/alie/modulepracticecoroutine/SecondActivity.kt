fbffcc1553f9ecfd739ed5dbcebfcf3bcafaa6c7
AliesYangpai <yangpai_beibei@163.com>
2021-09-11 13:59:58 +0800

    add condition of method pass param when adr of param change in other case

--- module_practice_coroutine/src/main/java/com/alie/modulepracticecoroutine/SecondActivity.kt
+++ module_practice_coroutine/src/main/java/com/alie/modulepracticecoroutine/SecondActivity.kt
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
+import com.alie.modulepracticecoroutine.data.CpuBean
 import com.alie.modulepracticecoroutine.repo.RepoComputer
 import kotlinx.android.synthetic.main.activity_second.*
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -19,6 +21,9 @@ import kotlinx.coroutines.withContext
 class SecondActivity : AppCompatActivity() {
 
     private val mRepoComputer by lazy { RepoComputer() }
+
+    private var mCpuBean = CpuBean("xx")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -35,27 +40,68 @@ class SecondActivity : AppCompatActivity() {
                     mRepoComputer.loadComputer()
                 }
                 println("launch computer end :${Thread.currentThread().name}")
-                mTvInfo.text = computer?.mCpuBean?.mName + computer?.mGpuBean?.mName+computer?.mMemoryBean?.mName
+                mTvInfo.text =
+                    computer?.mCpuBean?.mName + computer?.mGpuBean?.mName + computer?.mMemoryBean?.mName
             }
 
         }
         mBtnCpu.setOnClickListener {
             lifecycleScope.launch(Dispatchers.Main) {
-                withContext(Dispatchers.IO){mRepoComputer.loadCpuBean()}.let { mTvInfo.text = it?.mName }
+                withContext(Dispatchers.IO) { mRepoComputer.loadCpuBean() }.let {
+                    mTvInfo.text = it?.mName
+                }
             }
 
         }
         mBtnGpu.setOnClickListener {
-            lifecycleScope.launch  (Dispatchers.Main){
-                withContext(Dispatchers.IO){mRepoComputer.loadGpuBean()}.let { mTvInfo.text = it?.mName }
+            lifecycleScope.launch(Dispatchers.Main) {
+                withContext(Dispatchers.IO) { mRepoComputer.loadGpuBean() }.let {
+                    mTvInfo.text = it?.mName
+                }
             }
         }
         mBtnMemory.setOnClickListener {
             lifecycleScope.launch(Dispatchers.Main) {
-                withContext(Dispatchers.IO) {mRepoComputer.loadMemoryBean()}.let { mTvInfo.text = it?.mName }
+                withContext(Dispatchers.IO) { mRepoComputer.loadMemoryBean() }.let {
+                    mTvInfo.text = it?.mName
+                }
             }
         }
+        doCheckParamPassIn() // 验证~~~~
     }
 
 
+    /**
+     * 验证：
+     * 传参后，外部对象类型重新实例化后，已经传入方法的外部对象，在方法中是否也自动变成新的了
+     * SUMMARY: 方法内不会变，除非重新传入
+     * 原因：即便传入的是对象类型（实际上传入的是对象地址），那么在方法内部会【复制一份参数地址，也可以理解为对参数复制一份】，因此
+     * 方法内部操作的都是副本。外部对象地址变化后【注意 一定是地址变化，而不是解引用的变化】，如不再次调用用一下方法，则此时方法
+     * 中操作的对象地址仍是变化之前的地址
+     */
+    private fun doCheckParamPassIn() {
+        mBtnHashCode.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Main) {
+                showHashCode(mCpuBean)
+            }
+        }
+        mBtnGenerateNew.setOnClickListener {
+            generateCpuBean()
+        }
+    }
+
+    private suspend fun showHashCode(cpuBean: CpuBean) {
+        try {
+            println("===hashCode HashCode:${cpuBean.hashCode()}")
+            delay(2000)
+            println("===hashCode HashCode:${cpuBean.hashCode()}")
+        } catch (e: Exception) {
+            println("===hashCode msg:${e.message}")
+        }
+    }
+
+    fun generateCpuBean() {
+        mCpuBean = CpuBean("xxx")
+        println("===hashCode generateCpuBean:${mCpuBean.hashCode()}")
+    }
 }
