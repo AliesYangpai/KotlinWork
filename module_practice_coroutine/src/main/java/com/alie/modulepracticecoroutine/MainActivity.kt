ce28428bd1f00ff231a02f3215b75316fef84e40
Alie <yangpai_beibei@163.com>
2022-10-15 21:45:44 +0800

    add practice of coroutine---SDJ

--- module_practice_coroutine/src/main/java/com/alie/modulepracticecoroutine/MainActivity.kt
+++ module_practice_coroutine/src/main/java/com/alie/modulepracticecoroutine/MainActivity.kt
@@ -3,7 +3,10 @@ package com.alie.modulepracticecoroutine
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alie.modulepracticecoroutine.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.coroutineContext
 
 /**
  * GlobalScope:Process级别,activity/fragment销毁了也存在
@@ -12,10 +15,100 @@ import kotlinx.android.synthetic.main.activity_main.*
  * lifecycleScope：只能在activity、fragment中使用，绑定activity、fragment的生命周期
  */
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        mBtnJump.setOnClickListener { startActivity(Intent(this,SecondActivity::class.java)) }
+        setContentView(ActivityMainBinding.inflate(layoutInflater).let {
+            binding = it
+            it.root
+        })
+        binding.mBtnJump.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    SecondActivity::class.java
+                )
+            )
+        }
+        initTestWork()
     }
+
+    private fun initTestWork() {
+//        test01() // 协程异常-job()
+//        test02() // 协程异常-SupervisorJob()
+        test03() // 协程异常-supervisorScope{}
+    }
+
+
+    /**
+     * test01
+     * job()
+     * cancel 异常传递
+     */
+    private fun test01() {
+        val scope =
+            CoroutineScope(Job() + Dispatchers.Main + CoroutineExceptionHandler { coroutineContext, throwable ->
+                println(" test01 error ${throwable.message}")
+            })
+        scope.launch {
+            println("test01 1")
+            val data = 5 / 0
+        }
+        scope.launch {
+            println("test01 2")
+        }
+
+        scope.launch {
+            println("test01 3")
+        }
+        scope.launch {
+            println("test01 4")
+        }
+    }
+
+    /**
+     * test02
+     * Su
+     */
+    private fun test02() {
+        val scope =
+            CoroutineScope(SupervisorJob() + Dispatchers.Main + CoroutineExceptionHandler { coroutineContext, throwable ->
+                println(" test02 error ${throwable.message}")
+            })
+       val job = scope.launch {
+            println("test02 1")
+            val data = 5 / 0
+        }
+        scope.launch {
+            println("test02 2")
+        }
+
+        scope.launch {
+            println("test02 3")
+        }
+        scope.launch {
+            println("test02 4")
+        }
+
+
+        job.cancel()
+    }
+
+    private fun test03() {
+
+        CoroutineScope(Job() + Dispatchers.Main + CoroutineExceptionHandler { coroutineContext, throwable ->
+            println("test03 error ${throwable.message} thread:${Thread.currentThread().name}")
+        }).launch {
+            supervisorScope {
+                launch(Dispatchers.Default) {
+                    println("test03 1 thread:${Thread.currentThread().name}")
+                    val data = 3 / 0
+                }
+                println("test03 2 thread:${Thread.currentThread().name}")
+            }
+        }
+    }
+
+
 
 }
